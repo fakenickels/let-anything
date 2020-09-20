c056f47c6b9e7aeeb9bd0a6649793efa9f10b7a8
gtkatakura <gt.katakura@gmail.com>
2020-09-20 17:55:24 -0300

    fix: remove unnecessary any, move LetAnything to file scope

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,19 @@ type Config<T> = {
   let_: (value: T, continuation: ((value: T) => T)) => T,
 }
 
-export function letAnything<T>(config: Config<T>) {
-  interface LetAnything {
-    (gen: () => Generator<T, any, any>): T;
-    <TNext>(gen: () => Generator<T, any, TNext>): T;
-  }
+interface LetAnything<T> {
+  (gen: () => Generator<T, any, any>): T;
+  <TNext>(gen: () => Generator<T, any, TNext>): T;
+}
 
-  const lettified: LetAnything = (gen: () => Generator<T, any, any>) => {
+export function letAnything<T>(config: Config<T>): LetAnything<T> {
+  return (gen: () => Generator<T, any, any>) => {
     const context = gen()
 
     const compose: (step: IteratorResult<T>) => T = (step) => step.done
       ? step.value
-      : config.let_(step.value, value => compose(context.next(value as any))) // TODO: remove any
+      : config.let_(step.value, value => compose(context.next(value)))
 
     return compose(context.next())
   }
-
-  return lettified
 }
